c78912dca83e9457335cc034b7419e0bef7006d6
sebastianblassi <130934576+sebastianblassi@users.noreply.github.com>
2023-05-21 22:17:54 -0300

    Add files via upload

---
+++ javascrip.js
@@ -0,0 +1,15 @@
+let nombre = "nombre";
+let apellido = "apellido";
+
+
+console.log(nombre + " " + apellido);
+
+let numero1 = 1;
+let numero2 = 2;
+let suma = 1 + 2;
+
+console.log(suma);
+
+let numero3 = suma + 3;
+
+console.log(numero3);
